446bf172779d330cc3d8348d07432ba86a3d77a4
AlexAbS1993 <abetovale@yandex.ru>
2021-03-11 18:41:51 +0300

    setting is well done

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../main'
+
+const App:React.FC = () => {
+    return (
+        <div className="hello">
+
+        </div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+let isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name]_[hash].js' 
+    },
+    resolve:{
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.css']
+    },
+    module: {
+        rules:[
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.css/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.html/,
+                loader: 'raw-loader'
+              },
+        ]
+    },
+    plugins:[
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html')
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            linkType: 'text/css',
+            filename: '[name]_[hash].css'
+        })
+    ],
+    devServer: {
+        port: 4000,
+        open: true,
+        overlay: true
+    },
+    
+}
